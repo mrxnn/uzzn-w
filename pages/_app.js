9cff16da494005b31d737e6f66c010e7cc5d7391
Mayura <mayura.ramanayake@gmail.com>
2021-03-03 04:18:15 +0530

    Replaced sanity

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,3 @@
-import { ThemeProvider } from "styled-components";
-import { Globalstyle, theme } from "../styles/base";
-
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Globalstyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import Head from "next/head";
-import Header from "../components/Header";
-import Title from "../components/Title";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Branndland</title>
+        <title>Idk --- title</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <Title />
     </div>
   );
 }
