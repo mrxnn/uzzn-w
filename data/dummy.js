5d4c410dcf946bd21e00f24f4214897c3190d39c
Mayura <mayura.ramanayake@gmail.com>
2021-03-03 08:07:55 +0530

    test

--- data/dummy.js
+++ data/dummy.js
@@ -5,7 +5,7 @@ const data = [
       "Shift overall look and feel by adding these wonderful touches to furniture in your home",
     description:
       "React components outside of main will not be initialized by the browser. Do not add application logic here or custom CSS. If you need base components in your pages (like a menu or a toolbar), take a look at the App component instead.",
-    imageURL: "/images/6.png",
+    imageURL: "/images/7.jpg",
     avatar: "/images/maria.jpg",
     author: "Maria Yusefnova",
     publishedAt: "29 June 2021",
@@ -15,7 +15,7 @@ const data = [
     title: "Top 10 ideas on how to celebrate your own birthday",
     description:
       "The idea behind this class is that it allows you to have an optimal reading width. We added a new max-width by default called max-w-prose. If you are familiar with the typography plugin you might recognize this one. The idea behind this class is that it allows you to have an optimal reading width and height.",
-    imageURL: "/images/5.jpg",
+    imageURL: "/images/7.jpg",
     avatar: "/images/maria.jpg",
     author: "Maria Yusefnova",
     publishedAt: "12 December 2020",
