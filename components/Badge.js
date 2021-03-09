18d6b621380cf24caa066261c872d48abc28638f
Mayura <mayura.ramanayake@gmail.com>
2021-03-09 06:08:25 +0530

    Update navbar colors and sizing

--- components/Badge.js
+++ components/Badge.js
@@ -1,7 +1,11 @@
 export default function Badge({ text, classNames }) {
+  const isNumeric = !isNaN(text); // TRUE, if text is 123 or '123'
+
   const classes =
     classNames +
-    " bg-gray-400 rounded-lg font-semibold px-2 text-xs text-white h-5 flex justify-center items-center ml-2 mt-1";
+    (isNumeric ? " text-xs" : " text-xxs") +
+    " bg-gray-400 rounded-lg font-semibold px-2 text-white h-5 flex justify-center items-center ml-2 mt-1";
+
   return (
     <div className={classes}>
       <p>{text}</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,17 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
     fontFamily: {
       sans: ["Matter Trial"],
+    },
+    extend: {
+      colors: {
+        brown: "#A58861",
+        "brand-blue": "#0071FF",
+      },
+      fontSize: {
+        xxs: ["0.60rem", { lineHeight: "0rem" }],
+      },
     },
   },
   variants: {
