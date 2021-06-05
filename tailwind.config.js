778681e5daccc9855c28a091e162bb1a0cc6f2bd
Mayura Ramanayaka <mayura.ramanayake@gmail.com>
2021-06-05 11:38:12 +0530

    Removed unicode range

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,14 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      sans: ["TTCommons"],
-    },
     extend: {
-      // colors: {
-      //   brown: "#A58861",
-      //   "brand-blue": "#0071FF",
-      // },
-      // fontSize: {
-      //   xxx: ["0.50rem", { lineHeight: "0rem" }],
-      //   xxs: ["0.60rem", { lineHeight: "0rem" }],
-      // },
-      // letterSpacing: {
-      //   xwidest: "0.38em",
-      // },
+      fontFamily: {
+        sans: ["TT Commons", ...fontFamily.sans],
+      },
     },
   },
   variants: {
