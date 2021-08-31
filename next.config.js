ba0631c8cc88341892df0bcc2654e96c6d160978
Mayura <mayura.ramanayake@gmail.com>
2021-08-31 17:20:25 +0530

    Add featured section

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   images: {
     deviceSizes: [640, 768, 1024, 1280, 1536],
+    domains: ["s3.us-west-2.amazonaws.com"],
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
 import Layout from "../components/Layout";
 import Badge from "../components/Badge";
 import Thumbnail from "../components/Thumbnail";
+import { getDatabase } from "../lib/notion";
 
-export default function Home({ data }) {
+// blog posts database
+export const databaseId = process.env.NOTION_DATABASE;
+
+export default function Home({ posts }) {
+  const featured = posts
+    .filter((post) => {
+      const position = post.properties.Position.number;
+      return !!position; // filter out posts without a position
+    })
+    .map((post) => {
+      return {
+        category: post.properties.Category.rich_text[0].plain_text,
+        position: post.properties.Position.number,
+        imageURL: post.properties.Thumbnail.files[0].file.url,
+        title: post.properties.Title.title[0].plain_text,
+      };
+    })
+    .sort((post) => post.position)
+    .reverse();
+
   return (
     <Layout title="Inspiration, Creativity, Design">
       <div className="max-w-6xl mx-auto">
@@ -14,32 +34,11 @@ export default function Home({ data }) {
           </div>
         </div>
         <div className="grid grid-cols-4 grid-rows-2 mt-9 gap-8 h-[625px]">
-          <Thumbnail
-            variant="extended"
-            category="UI/UX"
-            imageURL="/images/3.jpg"
-            title="Visualize Team Usage With Sophisticated Usage Dashboard"
-          />
-          <Thumbnail
-            category="ILLUSTRATION"
-            title="Supercharge your Vercel Projects with Integrations"
-            imageURL="/images/4.jpg"
-          />
-          <Thumbnail
-            category="UI/UX"
-            title="$102M to Continue Building the Next Web, Together"
-            imageURL="/images/5.jpg"
-          />
-          <Thumbnail
-            category="UI/UX"
-            title="Next.js 11, Next.js Live and more: A recap of Next.js Conf Special Edition"
-            imageURL="/images/7.jpg"
-          />
-          <Thumbnail
-            category="MOTION"
-            title="How Core Web Vitals Will Impact Google Rankings in 2021"
-            imageURL="/images/9.jpg"
-          />
+          <Thumbnail {...featured[0]} variant="extended" />
+          <Thumbnail {...featured[1]} />
+          <Thumbnail {...featured[2]} />
+          <Thumbnail {...featured[3]} />
+          <Thumbnail {...featured[4]} />
         </div>
         <div className="flex justify-center items-center space-x-3 my-14">
           <p className="mt-[3px] font-medium text-gray-500">Scroll Down</p>
@@ -51,3 +50,14 @@ export default function Home({ data }) {
     </Layout>
   );
 }
+
+export const getStaticProps = async () => {
+  const database = await getDatabase(databaseId);
+
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 1,
+  };
+};
